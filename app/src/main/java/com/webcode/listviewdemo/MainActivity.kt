e4df51cf4406aeee189e6bf83fd2a9d943edc5f9
tanjidul <369tomu@gmail.com>
2024-04-24 10:08:55 +0600

    Backend conncetion

--- app/src/main/java/com/webcode/listviewdemo/MainActivity.kt
+++ app/src/main/java/com/webcode/listviewdemo/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.webcode.listviewdemo
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var listView:ListView
+
+    val user = arrayOf(
+        "Musfiqur Rahim", "Masrafi Bin Mortaza", "Sakib Al Hasan",
+        "Tamim Iqbal", "Mahmudullah Riad", "LIton Das",
+        "Taskin Ahmed","Rubel Hossain","Nazmul hossain"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        listView = findViewById(R.id.listView)
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,user)
+
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "Cliched Item"+ parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show()
+        }
         }
     }
-}
